087619daf8bd75a6e1ebe6cff2eb9a891a6bc108
LucasTomazProg <lucastomazc25@hotmail.com>
2025-08-28 15:22:50 -0300

    teste tela de login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,13 +8,13 @@ router.post('/login', async (req, res) => {
   const { email, senha } = req.body;
   if(!email || !senha) return res.status(400).json({ error: 'Informe email e senha' });
   const user = await prisma.usuario.findUnique({ where: { email } });
+  console.log('Usuario encontrado:', user);
   if (!user) return res.status(401).json({ error: 'Credenciais inválidas' });
   const ok = await bcrypt.compare(senha, user.senhaHash);
+  console.log('Senha hash no banco:', user?.senhaHash);
   if (!ok) return res.status(401).json({ error: 'Credenciais inválidas' });
   const token = jwt.sign({ sub: user.id, role: user.role }, process.env.JWT_SECRET || 'devsecret', { expiresIn: '8h' });
-  res.json({ token, user: { id: user.id, nome: user.nome, role: user.role } });
-  console.log('Usuario encontrado:', user);
-  console.log('Senha hash no banco:', user?.senhaHash);
+  res.json({ token, user: { id: user.id, nome: user.nome, role: user.role } });  
 });
 
 module.exports = router;
